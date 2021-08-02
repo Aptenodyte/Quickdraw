365ab8ab2fab23b5b3a978b1c9acb185a334b428
Aptenodyte <aptenodytezizou@gmail.com>
2021-08-02 18:03:00 -0500

    Updated linting configuration
    Switch to using Typescript Standard Style instead of custom ESLint
    configuration. Also added an .editorconfig file for future convenience.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 /**
  * Quickdraw - A NationStates utility to help quickly organize tag raids
- * Copyright (C) 2020  Zizou
+ * Copyright (C) 2021  Zizou
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
@@ -15,194 +15,192 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import * as ui from "./ui.js";
-import { SpyglassSheet } from "./sheet.js";
-import { Region, TargetInfo, initTargetFinder } from "./targetFinder.js";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+import * as ui from './ui.js'
+import { SpyglassSheet } from './sheet.js'
+import { Region, TargetInfo, initTargetFinder } from './targetFinder.js'
+import { Modal } from 'bootstrap'
 
 // Both Bootstrap and FileSaver.js are being included over CDN
-declare const bootstrap: typeof import("bootstrap");
-import { Modal } from "bootstrap";
-declare const { saveAs }: typeof import("file-saver");
+declare const bootstrap: typeof import('bootstrap')
+declare const { saveAs }: typeof import('file-saver')
 
-setup();
+setup()
 
-// The confirmation modal instance must be passed into this function. 
+// The confirmation modal instance must be passed into this function.
 // Otherwise, there is no way of closing it.
-function initModalUpdater(confirmationModal: Modal, updateLength: number, targetFinder: (prevTargAccepted: boolean) => TargetInfo | undefined) {
-  let i = 1;
+function initModalUpdater (confirmationModal: Modal, updateLength: number, targetFinder: (prevTargAccepted: boolean) => TargetInfo | undefined): (prevTargAccepted: boolean) => void {
+  let i = 1
 
-  let prevRegionUrl: string;
-  let prevRegionUpdateTime: string;
-  let prevTriggerName: string;
-  let prevTriggerUrl: string;
-  let prevTriggerLength: number;
+  let prevRegionUrl: string
+  let prevRegionUpdateTime: string
+  let prevTriggerName: string
+  let prevTriggerUrl: string
+  let prevTriggerLength: number
 
-  let raidFileText = "";
-  let triggerListText = "";
+  let raidFileText = ''
+  let triggerListText = ''
 
-  return function updateModal(prevTargAccepted: boolean) {
+  return function updateModal (prevTargAccepted: boolean) {
     if (prevTargAccepted) {
-      raidFileText += `${i}) ${prevRegionUrl} (${prevRegionUpdateTime})\n\ta) ${prevTriggerUrl} (${prevTriggerLength}s)\n\n`;
-      triggerListText += `${prevTriggerName}\n`;
-      i++;
+      raidFileText += `${i}) ${prevRegionUrl} (${prevRegionUpdateTime})\n\ta) ${prevTriggerUrl} (${prevTriggerLength}s)\n\n`
+      triggerListText += `${prevTriggerName}\n`
+      i++
     }
-    
-    const targetSearchResults = targetFinder(prevTargAccepted);
+
+    const targetSearchResults = targetFinder(prevTargAccepted)
     if (targetSearchResults === undefined) {
-      const downloadModalDiv = document.getElementById("raidDl")! as HTMLDivElement;
+      const downloadModalDiv = document.getElementById('raidDl')! as HTMLDivElement
       const downloadModal = new bootstrap.Modal(downloadModalDiv, {
-        backdrop: "static"
-      });
+        backdrop: 'static'
+      })
 
-      const dlRaidFile = document.getElementById("dlRaidFile")! as HTMLButtonElement;
-      const raidFileBlob = new Blob([raidFileText], {type: "text/plain;charset=utf-8"});
-      dlRaidFile.addEventListener("click", () => saveAs(raidFileBlob, "raidFile.txt"));
+      const dlRaidFile = document.getElementById('dlRaidFile')! as HTMLButtonElement
+      const raidFileBlob = new Blob([raidFileText], { type: 'text/plain;charset=utf-8' })
+      dlRaidFile.addEventListener('click', () => saveAs(raidFileBlob, 'raidFile.txt'))
 
-      const dlTriggerList = document.getElementById("dlTriggerList")! as HTMLButtonElement;
-      const triggerListBlob = new Blob([triggerListText], {type: "text/plain;charset=utf-8"});
-      dlTriggerList.addEventListener("click", () => saveAs(triggerListBlob, "trigger_list.txt"));
+      const dlTriggerList = document.getElementById('dlTriggerList')! as HTMLButtonElement
+      const triggerListBlob = new Blob([triggerListText], { type: 'text/plain;charset=utf-8' })
+      dlTriggerList.addEventListener('click', () => saveAs(triggerListBlob, 'trigger_list.txt'))
 
-      confirmationModal.hide();
-      downloadModal.show();
+      confirmationModal.hide()
+      downloadModal.show()
 
       // Reload the window after we're done downloading because every time
       // there will always be somebody who tries to run the program again after
       // running it once. When that happens, everything fucking breaks, an
       // orphan dies in a blood sacrifice ritual to a cult, and I burn my
       // fingers off in liquid nitrogen.
-      downloadModalDiv.addEventListener("hidden.bs.modal", () => window.location.reload());
+      downloadModalDiv.addEventListener('hidden.bs.modal', () => window.location.reload())
 
-      return;
+      return
     }
 
-    const { region, trigger } = targetSearchResults;
-    const regionUrl = `https://www.nationstates.net/region=${ui.sanitize(region.name)}`;
-    prevRegionUrl = regionUrl;
-    prevRegionUpdateTime = region.updateTimeString;
-    const triggerUrl = `https://www.nationstates.net/template-overall=none/region=${ui.sanitize(trigger.name)}`;
-    prevTriggerName = trigger.name;
-    prevTriggerUrl = triggerUrl;
-    const triggerLength = region.updateTime - trigger.updateTime;
-    prevTriggerLength = triggerLength;
+    const { region, trigger } = targetSearchResults
+    const regionUrl = `https://www.nationstates.net/region=${ui.sanitize(region.name)}`
+    prevRegionUrl = regionUrl
+    prevRegionUpdateTime = region.updateTimeString
+    const triggerUrl = `https://www.nationstates.net/template-overall=none/region=${ui.sanitize(trigger.name)}`
+    prevTriggerName = trigger.name
+    prevTriggerUrl = triggerUrl
+    const triggerLength = region.updateTime - trigger.updateTime
+    prevTriggerLength = triggerLength
 
-    window.open(regionUrl, "_blank");
-    ui.updateModal(Math.round((region.regionNumber / updateLength) * 100), i, regionUrl, region.updateTimeString, triggerUrl, triggerLength);
-  };
+    window.open(regionUrl, '_blank')
+    ui.updateModal(Math.round((region.regionNumber / updateLength) * 100), i, regionUrl, region.updateTimeString, triggerUrl, triggerLength)
+  }
 }
 
-async function main(ev: Event) {
-  ev.preventDefault();
+async function main (ev: Event): Promise<void> {
+  ev.preventDefault()
 
-  const updateSelector = document.getElementById("updateTime")! as HTMLSelectElement;
-  const updatePeriod = updateSelector.value;
-  if (updatePeriod === "Choose update")
-    throw new Error("Please select an update");
+  const updateSelector = document.getElementById('updateTime')! as HTMLSelectElement
+  const updatePeriod = updateSelector.value
+  if (updatePeriod === 'Choose update') { throw new Error('Please select an update') }
 
-  const endoCountInput = document.getElementById("endoCount")! as HTMLInputElement;
-  const endoCount = +endoCountInput.value;
-  const embassyFiltersInput = document.getElementById("ignoreEmbassies")! as HTMLInputElement;
-  const embassyFilters = embassyFiltersInput.value.split(",").map(filter => filter.trim().toLowerCase());
-  const wfeFiltersInput = document.getElementById("ignorePhrases")! as HTMLInputElement;
-  const wfeFilters = wfeFiltersInput.value.split(",").map(filter => filter.trim());
+  const endoCountInput = document.getElementById('endoCount')! as HTMLInputElement
+  const endoCount = +endoCountInput.value
+  const embassyFiltersInput = document.getElementById('ignoreEmbassies')! as HTMLInputElement
+  const embassyFilters = embassyFiltersInput.value.split(',').map(filter => filter.trim().toLowerCase())
+  const wfeFiltersInput = document.getElementById('ignorePhrases')! as HTMLInputElement
+  const wfeFilters = wfeFiltersInput.value.split(',').map(filter => filter.trim())
 
-  const doApplyWfeFilters = (wfeFilters.length === 1) && (wfeFilters[0] === "");
-  const doApplyEmbassyFilters = (embassyFilters.length === 1) && (embassyFilters[0] === "");
+  const doApplyWfeFilters = (wfeFilters.length === 1) && (wfeFilters[0] === '')
+  const doApplyEmbassyFilters = (embassyFilters.length === 1) && (embassyFilters[0] === '')
 
-  const spyglassSheetInput = document.getElementById("spyglassSheetInput")! as HTMLInputElement;
-  if (spyglassSheetInput.files!.length !== 1)
-    throw new Error("Incorrect amount of files provided");
-  const spyglassSheet = await SpyglassSheet.init(spyglassSheetInput.files![0]);
-  
-  const nameColumn = "A";
-  const updateTimeColumn = (updatePeriod === "major") ? "F" : "E";
-  const delEndosColumn = "H";
-  const embassiesColumn = "I";
-  const wfeColumn = "J";
-  const regionArray = [];
+  const spyglassSheetInput = document.getElementById('spyglassSheetInput')! as HTMLInputElement
+  if (spyglassSheetInput.files!.length !== 1) { throw new Error('Incorrect amount of files provided') }
+  const spyglassSheet = await SpyglassSheet.init(spyglassSheetInput.files![0])
+
+  const nameColumn = 'A'
+  const updateTimeColumn = (updatePeriod === 'major') ? 'F' : 'E'
+  const delEndosColumn = 'H'
+  const embassiesColumn = 'I'
+  const wfeColumn = 'J'
+  const regionArray = []
   for (let i = 2; i < spyglassSheet.sheetLength; i++) {
-    const regionNameCell = spyglassSheet.readCell(`${nameColumn}${i}`) as string;
-    let regionName: string;
-    const regionUpdateTime = spyglassSheet.readTimeInSeconds(`${updateTimeColumn}${i}`);
-    const regionUpdateTimeString = spyglassSheet.readCell(`${updateTimeColumn}${i}`) as string;
+    const regionNameCell = spyglassSheet.readCell(`${nameColumn}${i}`) as string
+    let regionName: string
+    const regionUpdateTime = spyglassSheet.readTimeInSeconds(`${updateTimeColumn}${i}`)
+    const regionUpdateTimeString = spyglassSheet.readCell(`${updateTimeColumn}${i}`) as string
 
-    if (regionNameCell.slice(-1) === "~" || regionNameCell.slice(-1) === "*")
-      regionName = regionNameCell.slice(0, -1);
-    else
-      regionName = regionNameCell;
-    if (regionNameCell.slice(-1) !== "~") {
-      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, false));
-      continue;
+    if (regionNameCell.slice(-1) === '~' || regionNameCell.slice(-1) === '*') { regionName = regionNameCell.slice(0, -1) } else { regionName = regionNameCell }
+    if (regionNameCell.slice(-1) !== '~') {
+      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, false))
+      continue
     }
 
-    const delEndos = +(spyglassSheet.readCell(`${delEndosColumn}${i}`) as string);
+    const delEndos = +(spyglassSheet.readCell(`${delEndosColumn}${i}`) as string)
     if (endoCount <= delEndos) {
-      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, false));
-      continue;
+      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, false))
+      continue
     }
 
     if (!doApplyWfeFilters) {
-      const regionWfe = spyglassSheet.readCell(`${wfeColumn}${i}`) as string;
+      const regionWfe = spyglassSheet.readCell(`${wfeColumn}${i}`) as string
       // Ensure that no issues are cause by empty cells
       if (regionWfe !== undefined) {
-        let isHittable = true;
-        for (const filter of wfeFilters)
+        let isHittable = true
+        for (const filter of wfeFilters) {
           if (regionWfe.includes(filter)) {
-            isHittable = false;
-            break;
+            isHittable = false
+            break
           }
+        }
         if (!isHittable) {
-          regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, false));
-          continue;
+          regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, false))
+          continue
         }
       }
     }
 
     if (!doApplyEmbassyFilters) {
-      const regionEmbassiesString = spyglassSheet.readCell(`${embassiesColumn}${i}`) as string;
+      const regionEmbassiesString = spyglassSheet.readCell(`${embassiesColumn}${i}`) as string
       // Fix glitch caused by empty embassy cells on regions without embassies
       if (regionEmbassiesString === undefined) {
-        regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, true));
-        continue;
+        regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, true))
+        continue
       }
-      const regionEmbassies = regionEmbassiesString.split(",").map(embassy => embassy.toLowerCase());
-      const forbiddenEmbassies = embassyFilters.filter(embassy => regionEmbassies.includes(embassy));
-      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, forbiddenEmbassies.length === 0));
+      const regionEmbassies = regionEmbassiesString.split(',').map(embassy => embassy.toLowerCase())
+      const forbiddenEmbassies = embassyFilters.filter(embassy => regionEmbassies.includes(embassy))
+      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, forbiddenEmbassies.length === 0))
     } else {
-      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, true));
+      regionArray.push(new Region(i - 1, regionName, regionUpdateTime, regionUpdateTimeString, true))
     }
   }
 
-  const targetFinder = initTargetFinder(regionArray);
-  const targetConfirmationModalDiv = document.getElementById("targetConfirm")! as HTMLDivElement;
+  const targetFinder = initTargetFinder(regionArray)
+  const targetConfirmationModalDiv = document.getElementById('targetConfirm')! as HTMLDivElement
   const targetConfirmationModal = new bootstrap.Modal(targetConfirmationModalDiv, {
-    backdrop: "static",
+    backdrop: 'static',
     keyboard: false
-  });
-  const updateModal = initModalUpdater(targetConfirmationModal, regionArray.length, targetFinder);
-  const acceptTargetButton = document.getElementById("acceptTarget")! as HTMLButtonElement;
-  const declineTargetButton = document.getElementById("declineTarget")! as HTMLButtonElement;
-  acceptTargetButton.addEventListener("click", updateModal.bind(null, true));
-  declineTargetButton.addEventListener("click", updateModal.bind(null, false));
+  })
+  const updateModal = initModalUpdater(targetConfirmationModal, regionArray.length, targetFinder)
+  const acceptTargetButton = document.getElementById('acceptTarget')! as HTMLButtonElement
+  const declineTargetButton = document.getElementById('declineTarget')! as HTMLButtonElement
+  acceptTargetButton.addEventListener('click', updateModal.bind(null, true))
+  declineTargetButton.addEventListener('click', updateModal.bind(null, false))
 
-  updateModal(false);
-  targetConfirmationModal.show();
+  updateModal(false)
+  targetConfirmationModal.show()
 }
 
-function setup() {
+function setup (): void {
   // Ensure that all uncaught errors can be properly displayed to the user
-  window.onerror = function(message, url, line, col) {
-    alert(`${message}\n At ${line}:${col} of ${url}\n Please check the console for the full error`);
-  };
+  window.onerror = function (message, url, line, col) {
+    alert(`${message as string}\n At ${line as number}:${col as number} of ${url as string}\n Please check the console for the full error`)
+  }
   // And unhandled promise rejections too
   window.onunhandledrejection = (event: PromiseRejectionEvent) => {
-    alert(`${event.reason}\n Please check the console for the full error`);
-  };
+    alert(`${event.reason as string}\n Please check the console for the full error`)
+  }
 
-  const fillDefaultEmbassiesButton = document.getElementById("embassyDefaults")! as HTMLInputElement;
-  const fillDefaultPhrasesButton = document.getElementById("phraseDefaults")! as HTMLInputElement;
-  fillDefaultEmbassiesButton.addEventListener("click", ui.fillDefaultEmbassies);
-  fillDefaultPhrasesButton.addEventListener("click", ui.fillDefaultPhrases);
-  
-  const mainForm = document.getElementById("mainForm")! as HTMLFormElement;
-  mainForm.addEventListener("submit", main);
+  const fillDefaultEmbassiesButton = document.getElementById('embassyDefaults')! as HTMLInputElement
+  const fillDefaultPhrasesButton = document.getElementById('phraseDefaults')! as HTMLInputElement
+  fillDefaultEmbassiesButton.addEventListener('click', ui.fillDefaultEmbassies)
+  fillDefaultPhrasesButton.addEventListener('click', ui.fillDefaultPhrases)
+
+  const mainForm = document.getElementById('mainForm')! as HTMLFormElement
+  mainForm.addEventListener('submit', main) // eslint-disable-line @typescript-eslint/no-misused-promises
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,6 +1,6 @@
 /**
  * Quickdraw - A NationStates utility to help quickly organize tag raids
- * Copyright (C) 2020  Zizou
+ * Copyright (C) 2021  Zizou
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
@@ -15,43 +15,45 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 /**
  * Fills the default embassy filters for the "Ignore Embassies" field
  */
-export function fillDefaultEmbassies(): void {
+export function fillDefaultEmbassies (): void {
   const DEFAULT_EMBASSY_FILTERS = [
-    "The Black Hawks", 
-    "Doll Guldur", 
-    "Frozen Circle", 
-    "3 Guys", 
-    "Plum Island",
-    "The Autumnal Court"
-  ];
-  const ignoreEmbassiesField = document.getElementById("ignoreEmbassies")! as HTMLInputElement;
-  ignoreEmbassiesField.value = DEFAULT_EMBASSY_FILTERS.join(", ");
+    'The Black Hawks',
+    'Doll Guldur',
+    'Frozen Circle',
+    '3 Guys',
+    'Plum Island',
+    'The Autumnal Court'
+  ]
+  const ignoreEmbassiesField = document.getElementById('ignoreEmbassies')! as HTMLInputElement
+  ignoreEmbassiesField.value = DEFAULT_EMBASSY_FILTERS.join(', ')
 }
 
 /**
  * Fills the default WFE filters for the "Ignore phrases" field
  */
-export function fillDefaultPhrases(): void {
+export function fillDefaultPhrases (): void {
   const DEFAULT_PHRASE_FILTERS = [
-    "https://www.forum.the-black-hawks.org", // Exclude the TBH forums
-    "http://forum.theeastpacific.com", // Exclude the TEP forums
-    "https://www.nationstates.net/page=dispatch/id=485374", // Exclude the NPA advertisement dispatch
-    "https://discord.gg/XWvERyc", // Exclude the TBH Discord server
-    "https://forum.thenorthpacific.org", // Exclude the TNP forums
-    "https://discord.gg/Tghy5kW", // Exclude the 3 Guys Discord
-    "https://www.westpacific.org", // Exclude TWP forums
-    "https://discord.gg/y4wrfg8", // Exclude Lily Discord
-    "https://discord.gg/m7qW9AS", // Exclude alt Lily Discord
-    "https://lilystates.proboards.com", // Exclude Lily forums
-    "https://forums.europeians.com/index.php", // Exclude the Euro forums
-    "https://discord.gg/nYAwZ7f" // Exclude Euro Discord
-  ];
+    'https://www.forum.the-black-hawks.org', // Exclude the TBH forums
+    'http://forum.theeastpacific.com', // Exclude the TEP forums
+    'https://www.nationstates.net/page=dispatch/id=485374', // Exclude the NPA advertisement dispatch
+    'https://discord.gg/XWvERyc', // Exclude the TBH Discord server
+    'https://forum.thenorthpacific.org', // Exclude the TNP forums
+    'https://discord.gg/Tghy5kW', // Exclude the 3 Guys Discord
+    'https://www.westpacific.org', // Exclude TWP forums
+    'https://discord.gg/y4wrfg8', // Exclude Lily Discord
+    'https://discord.gg/m7qW9AS', // Exclude alt Lily Discord
+    'https://lilystates.proboards.com', // Exclude Lily forums
+    'https://forums.europeians.com/index.php', // Exclude the Euro forums
+    'https://discord.gg/nYAwZ7f' // Exclude Euro Discord
+  ]
 
-  const ignorePhrasesField = document.getElementById("ignorePhrases")! as HTMLInputElement;
-  ignorePhrasesField.value = DEFAULT_PHRASE_FILTERS.join(", ");
+  const ignorePhrasesField = document.getElementById('ignorePhrases')! as HTMLInputElement
+  ignorePhrasesField.value = DEFAULT_PHRASE_FILTERS.join(', ')
 }
 
 /**
@@ -59,20 +61,19 @@ export function fillDefaultPhrases(): void {
  * @param bar The progress bar to alter
  * @param percentage The percentage of the progress bar that should be filled
  */
-export function updateProgressBar(bar: HTMLDivElement, percentage: number): void {
-  bar.style.width = `${percentage}%`;
-  bar.innerText = `${percentage}%`;
-  bar.setAttribute("aria-valuenow", percentage.toString());
-  if (percentage === 100)
-    bar.classList.remove("progress-bar-striped", "progress-bar-animated");
+export function updateProgressBar (bar: HTMLDivElement, percentage: number): void {
+  bar.style.width = `${percentage}%`
+  bar.innerText = `${percentage}%`
+  bar.setAttribute('aria-valuenow', percentage.toString())
+  if (percentage === 100) { bar.classList.remove('progress-bar-striped', 'progress-bar-animated') }
 }
 
 /**
  * A function that has an asynchronous delay of the given amount of milliseconds
  * @param ms The milliseconds to delay for
  */
-export async function delay(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+export async function delay (ms: number): Promise<void> {
+  return await new Promise(resolve => setTimeout(resolve, ms))
 }
 
 /**
@@ -84,25 +85,25 @@ export async function delay(ms: number): Promise<void> {
  * @param triggerUrl The url of the trigger
  * @param triggerLen The lenght of the trigger
  */
-export function updateModal(updatePercentage: number, targetNumber: number, targetUrl: string, targetUpdate: string, triggerUrl: string, triggerLen: number): void {
-  const updateProgress = document.getElementById("updateProgress")! as HTMLSpanElement;
-  updateProgress.innerText = updatePercentage.toString();
-  const targetNum = document.getElementById("targetNum")! as HTMLSpanElement;
-  targetNum.innerText = targetNumber.toString();
+export function updateModal (updatePercentage: number, targetNumber: number, targetUrl: string, targetUpdate: string, triggerUrl: string, triggerLen: number): void {
+  const updateProgress = document.getElementById('updateProgress')! as HTMLSpanElement
+  updateProgress.innerText = updatePercentage.toString()
+  const targetNum = document.getElementById('targetNum')! as HTMLSpanElement
+  targetNum.innerText = targetNumber.toString()
 
-  const targetLinkAnchor = document.getElementById("targetLinkAnchor")! as HTMLAnchorElement;
-  targetLinkAnchor.textContent = targetUrl;
-  targetLinkAnchor.href = targetUrl;
-  const targetTime = document.getElementById("targetTime")! as HTMLSpanElement;
-  targetTime.innerText = targetUpdate;
+  const targetLinkAnchor = document.getElementById('targetLinkAnchor')! as HTMLAnchorElement
+  targetLinkAnchor.textContent = targetUrl
+  targetLinkAnchor.href = targetUrl
+  const targetTime = document.getElementById('targetTime')! as HTMLSpanElement
+  targetTime.innerText = targetUpdate
 
-  const triggerLinkAnchor = document.getElementById("triggerLinkAnchor")! as HTMLAnchorElement;
-  triggerLinkAnchor.textContent = triggerUrl;
-  triggerLinkAnchor.href = triggerUrl;
-  const triggerLength = document.getElementById("triggerLength")! as HTMLSpanElement;
-  triggerLength.innerText = triggerLen.toString();
+  const triggerLinkAnchor = document.getElementById('triggerLinkAnchor')! as HTMLAnchorElement
+  triggerLinkAnchor.textContent = triggerUrl
+  triggerLinkAnchor.href = triggerUrl
+  const triggerLength = document.getElementById('triggerLength')! as HTMLSpanElement
+  triggerLength.innerText = triggerLen.toString()
 }
 
-export function sanitize(text: string): string {
-  return text.trim().replace(/ /g, "_").toLowerCase();
+export function sanitize (text: string): string {
+  return text.trim().replace(/ /g, '_').toLowerCase()
 }
